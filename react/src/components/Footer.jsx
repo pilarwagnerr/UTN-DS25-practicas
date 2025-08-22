96bdf6e80933af74d304e99ef3c486746ba7e764
Pilar Wagner <pilarwagner94@gmail.com>
2025-08-22 10:08:12 -0300

    Update Footer.jsx

--- react/src/components/Footer.jsx
+++ react/src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <div id="pie">
             <p>
-                © 2025 Librería El Ateneo. — 
+                © 2025 Librería El Ateneo.
                 <span id="fb"> FACEBOOK</span>: elateneograndsplendid
                 <span id="ig"> INSTAGRAM</span>: elateneograndsplendid
             </p>
